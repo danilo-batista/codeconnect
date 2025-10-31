8b34db8e02bdbc0168bf3ea2c90667c4b473b6a7
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-10-31 16:05:59 -0300

    fix: correct a Router import with wrong word.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AppRouter } from "./Router/index.jsx";
+import { AppRouter } from "./router/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
