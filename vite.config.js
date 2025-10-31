04a22d0e9c58df7335521cc132f5a2e4bb4e7ad6
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-10-31 10:44:53 -0300

    feat: 1st class - config router.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	build: {
+		outDir: "dist",
+		minify: "esbuild",
+		sourcemap: false,
+		target: "esnext",
+		cssCodeSplit: true,
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
+	server: {
+		historyApiFallback: true,
+	},
+});
